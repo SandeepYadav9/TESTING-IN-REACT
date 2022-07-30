e157e8f9897a1bc845277c802bff0eeb3a034f2f
Sandeep Yadav <sandeepuim10@gmail.com>
2022-07-30 13:46:29 +0530

    TESTING DATA

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  //Arrange Data
   render(<App />);
+  //Act
+
+  //Assert
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
